5346855eb088ae3a223f356f9d08f762d7d50511
alexandru.savoiu <alexandru.savoiu@resmed.com.au>
2019-01-11 16:19:23 +0200

    -add geolocation support

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./util/message');
+const {generateMessage, generateLocationMessage} = require('./util/message');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -25,6 +25,11 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text))
         callback('This is from the server');
+    });
+
+
+    socket.on('createLocationMessage', (cords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', cords.latitude, cords.longitude));
     });
 
     socket.on('disconnect', () => {
--- server/util/message.test.js
+++ server/util/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,6 +11,22 @@ describe('generateMessage', () => {
         expect(res).toInclude({
             from: expectedFrom,
             text: expectedText
+        });
+        expect(res.createdAt).toBeA('number');
+    })
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var expectedFrom = 'test user';
+        var expectedLatitude = 1;
+        var expectedLongitude = 2;
+
+        var res = generateLocationMessage(expectedFrom, expectedLatitude, expectedLongitude);
+
+        expect(res).toInclude({
+            from: expectedFrom,
+            url: 'https://www.google.com/maps?q=1,2'
         });
         expect(res.createdAt).toBeA('number');
     })
